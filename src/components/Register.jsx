89bbd68238ddce2c7131ff4cb8d9904cce9ba2be
Anirudh CK <anirudhckhowin@gmail.com>
2025-03-06 21:44:52 +0530

    #008

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -35,7 +35,7 @@ function Register({ isOpen, onClose }) {
     >
       <div className="bg-white rounded-lg p-6 w-96 shadow-lg relative">
         {/* Header */}
-        <div className="flex justify-between items-center border-b pb-2">
+        <div className="flex justify-between items-center  pb-2">
           <h2 className="text-xl font-semibold">Register</h2>
           <button onClick={onClose} className="text-gray-500 hover:text-gray-700">
             <IoMdClose size={24} />
